1a55e85df42ab907f4c987212799d19782fb7b4e
Jan-PC <Laquat@gmx.net>
2018-12-19 16:47:25 +0100

    Initial

---
+++ app/src/main/java/com/example/jan_pc/rescueu_2_1/hauptmenueActivity.java
@@ -0,0 +1,37 @@
+package com.example.jan_pc.rescueu_2_1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class hauptmenueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hauptmenue);
+    }
+
+
+    public void settingsBtn(View view){
+
+
+        Intent intent = new Intent(this,SettingsActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void mapBtn (View view){
+        Intent intent = new Intent (this, MapsActivity.class);
+        startActivity(intent);
+
+        }
+
+    public void notfallBtn (View view){
+        Intent intent = new Intent (this, notfallActicity.class);
+        startActivity(intent);
+
+    }
+
+}
